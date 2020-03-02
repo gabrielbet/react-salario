9453dbb23316fa7c942c271cde6692f576730d39
gabrielbet <55458497+gabrielbet@users.noreply.github.com>
2020-03-01 22:31:37 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {LabelsInput} from './LabelsInput';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super();
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return ( 
+      <div className="App">
+        <h1>Cálculo de salário com React</h1>
+        <LabelsInput />
+      </div>
+    );
+  }
 }
 
 export default App;
